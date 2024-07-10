a13ede7e3a2edeb828d79805c1693142363d776d
Diego Luna <dpl@cesar.school>
2024-07-09 22:43:24 -0300

    feat: init footer component and add to layout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import { Outlet } from 'react-router-dom'
 
@@ -5,6 +6,7 @@ const BaseLayout = () => (
   <div className="flex flex-col min-h-screen">
     <Navbar />
     <Outlet />
+    <Footer />
   </div>
 )
 
