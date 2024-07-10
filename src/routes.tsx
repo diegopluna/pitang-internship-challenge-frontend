d39b850155512b1175c59924323ec06969fdd24a
Diego Luna <dpl@cesar.school>
2024-07-09 21:13:33 -0300

    feat: add base pages layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,15 @@
 import { lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import BaseLayout from './layouts/BaseLayout'
 
 const Home = lazy(() => import('./pages/Home'))
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<BaseLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 )
