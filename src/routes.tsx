42f197a0137eea938e58d70a5dec02a50f46535a
Diego Luna <dpl@cesar.school>
2024-07-09 22:09:36 -0300

    feat: add routes for scheduling and viewing appointments

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,16 @@
-import { lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import BaseLayout from './layouts/BaseLayout'
-
-const Home = lazy(() => import('./pages/Home'))
+import Home from './pages/Home'
+import Schedule from './pages/Schedule'
+import ListAppointments from './pages/ListAppointments'
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route element={<BaseLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/list-appointments" element={<ListAppointments />} />
       </Route>
     </Routes>
   </BrowserRouter>
