0ead2f3d696979d0790d7966e2e2638f0dd1e5e3
Diego Luna <dpl@cesar.school>
2024-07-13 16:26:50 -0300

    feat: improve date-utils logic

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -5,13 +5,17 @@ export const getLocalizedTime = (hours: number, minutes: number = 0): Date => {
 }
 
 export const getMinTime = (selectedDate: Date | null): Date => {
-  const now = new Date()
-  const minTime = getLocalizedTime(9) // 9 AM
+  const now = new Date();
+  const minTime = getLocalizedTime(9); // 9 AM
 
   if (selectedDate && selectedDate.toDateString() === now.toDateString()) {
-    return now > minTime ? now : minTime
+    if (now > minTime) {
+      const nextHour = new Date(now);
+      nextHour.setUTCHours(now.getUTCHours() + 1, 0, 0, 0);
+      return nextHour;
+    }
   }
-  return minTime
+  return minTime;
 }
 
 export const getMaxTime = (): Date => {
@@ -20,18 +24,15 @@ export const getMaxTime = (): Date => {
 
 export const getMinDate = (): Date => {
   const now = new Date()
-  console.log('now', now)
   const maxTime = getMaxTime()
-  console.log('maxTime', maxTime)
   const minTime = getLocalizedTime(9) // 9 AM
-  console.log('minTime', minTime)
 
   if (now > maxTime) {
     now.setUTCDate(now.getUTCDate() + 1)
     now.setUTCHours(9, 0, 0, 0)
   } else if (now < minTime) {
     now.setUTCHours(9, 0, 0, 0)
-  } else if (now >= minTime) {
+  } else {
     now.setUTCHours(now.getUTCHours() + 1, 0, 0, 0)
   }
 
