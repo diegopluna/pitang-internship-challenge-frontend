e598d140fbbf95ddacc9fe50d80291b4e9eac0c8
Diego Luna <dpl@cesar.school>
2024-07-13 23:27:28 -0300

    feat: add react query devtools for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import './index.css'
 import AppRoutes from './routes.tsx'
 import { ModalProvider } from './contexts/ModalContext.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
+
+// TODO: Add 404 Route
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ModalProvider>
         <AppRoutes />
+        <ReactQueryDevtools />
       </ModalProvider>
     </QueryClientProvider>
   </React.StrictMode>,
