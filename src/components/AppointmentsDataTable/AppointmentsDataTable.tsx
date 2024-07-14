872427eed0d46b30b7db89bcac27b7df1ce7e27f
Diego Luna <dpl@cesar.school>
2024-07-13 23:05:39 -0300

    feat: add appointments table

---
+++ src/components/AppointmentsDataTable/AppointmentsDataTable.tsx
@@ -0,0 +1,114 @@
+import * as React from 'react'
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from '@tanstack/react-table'
+
+import { Table, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+
+import { Skeleton } from '@/components/ui/skeleton'
+import TableFilters from './TableFilters'
+import TableBodyComponent from './TableBody'
+import TablePagination from './TablePagination'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  )
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+    },
+  })
+
+  return (
+    <div>
+      <div className="rounded-md border">
+        <TableFilters table={table} />
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBodyComponent table={table} />
+        </Table>
+      </div>
+      <TablePagination table={table} />
+    </div>
+  )
+}
+
+export function DataTableSkeleton() {
+  return (
+    <div>
+      <div className="rounded-md border">
+        <div className="px-6 py-4 border-b">
+          <div className="flex items-center justify-between gap-4">
+            <Skeleton className="h-10 w-[200px]" />
+            <Skeleton className="h-10 w-[200px]" />
+            <Skeleton className="h-10 w-[200px]" />
+          </div>
+        </div>
+        <div className="p-4">
+          <div className="space-y-2">
+            {[...Array(5)].map((_, index) => (
+              <div key={index} className="flex items-center space-x-4">
+                <Skeleton className="h-12 w-[100px]" />
+                <Skeleton className="h-12 w-[200px]" />
+                <Skeleton className="h-12 w-[150px]" />
+                <Skeleton className="h-12 w-[100px]" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Skeleton className="h-8 w-[100px]" />
+        <Skeleton className="h-8 w-[80px]" />
+        <Skeleton className="h-8 w-[80px]" />
+      </div>
+    </div>
+  )
+}
