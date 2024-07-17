30ae036ebb7c553df347f34f1b872f596f8bc41f
Diego Peter <dpl@cesar.school>
2024-07-17 00:17:12 -0300

    feat: wrap the toast provider on the app entry file

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import AppRoutes from './routes.tsx'
 import { ModalProvider } from './contexts/ModalContext.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ToastContextProvider } from './contexts/ToastContext.tsx'
 
 const queryClient = new QueryClient()
 
@@ -13,8 +14,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ModalProvider>
-        <AppRoutes />
-        <ReactQueryDevtools />
+        <ToastContextProvider>
+          <AppRoutes />
+          <ReactQueryDevtools buttonPosition="bottom-left" />
+        </ToastContextProvider>
       </ModalProvider>
     </QueryClientProvider>
   </React.StrictMode>,
