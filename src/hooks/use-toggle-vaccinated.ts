95d9cedff89ec593308024ace52f5af1de93e49f
Diego Peter <dpl@cesar.school>
2024-07-17 00:21:00 -0300

    feat: show toast when toggling vaccination status

--- src/hooks/use-toggle-vaccinated.ts
+++ src/hooks/use-toggle-vaccinated.ts
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import api, { handleApiError } from '@/utils/api'
 import { useQueryClient } from '@tanstack/react-query'
+import { useToast } from '@/contexts/ToastContext'
 
 
 export const useToggleVaccinated = () => {
+  const toast = useToast()
   const [isLoading, setIsLoading] = useState(false)
   const queryClient = useQueryClient()
 
@@ -12,7 +14,9 @@ export const useToggleVaccinated = () => {
     try {
       await api.patch(`/appointments/${id}/toggle-vaccinated`)
       queryClient.invalidateQueries({ queryKey: ['appointments'] })
+      toast.success('Status de vacinação atualizado com sucesso!')
     } catch( error) {
+      toast.error('Erro ao atualizar status de vacinação!')
       throw handleApiError(error)
     } finally {
       setIsLoading(false)
