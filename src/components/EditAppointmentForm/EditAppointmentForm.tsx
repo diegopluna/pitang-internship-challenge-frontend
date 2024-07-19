2cb760cce4efc052343952827362adb3dfb31921
Diego Peter <dpl@cesar.school>
2024-07-19 01:24:54 -0300

    docs: document timezone prop in edit appointment form

--- src/components/EditAppointmentForm/EditAppointmentForm.tsx
+++ src/components/EditAppointmentForm/EditAppointmentForm.tsx
@@ -54,6 +54,7 @@ const EditAppointmentForm = ({ appointment }: { appointment: Appointment }) => {
               <CustomDatePicker
                 /// Birthday cannot be in the future
                 futureYears={0}
+                // Birthday is always at midnight UTC
                 timeZone="UTC"
                 maxDate={new Date()}
                 selected={field.value}
