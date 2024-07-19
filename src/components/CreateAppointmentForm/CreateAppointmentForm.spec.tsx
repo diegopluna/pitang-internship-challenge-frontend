c4da6bcd292a27a46110313f5acb0c3081e39c29
Diego Peter <dpl@cesar.school>
2024-07-18 23:06:24 -0300

    test: adjust create appointment form test to account for the new useMutate hook

--- src/components/CreateAppointmentForm/CreateAppointmentForm.spec.tsx
+++ src/components/CreateAppointmentForm/CreateAppointmentForm.spec.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@/utils/customRender'
 import userEvent from '@testing-library/user-event'
 
 import { ModalProvider } from '@/contexts/ModalContext'
@@ -7,6 +7,7 @@ import CreateAppointmentForm from './CreateAppointmentForm'
 
 vi.mock('react-router-dom', () => ({
   useNavigate: () => vi.fn(),
+  MemoryRouter: ({ children }: { children: React.ReactNode }) => children,
 }))
 
 describe('<CreateAppointmentForm />', () => {
